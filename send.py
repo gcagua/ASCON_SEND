7bc6f5c2c454d3dd6b7696b22312c2aea8ade2be
Gabriela Cagua <69643772+gcagua@users.noreply.github.com>
2023-12-05 00:52:34 -0500

    Add files via upload

---
+++ send.py
@@ -0,0 +1,162 @@
+import socket
+
+def ascon_encrypt(key, nonce, associateddata, plaintext, variant="Ascon-128"): 
+    assert variant in ["Ascon-128", "Ascon-128a", "Ascon-80pq"]
+    if variant in ["Ascon-128", "Ascon-128a"]: assert(len(key) == 16 and len(nonce) == 16)
+    if variant == "Ascon-80pq": assert(len(key) == 20 and len(nonce) == 16)
+    S = [0, 0, 0, 0, 0]
+    k = len(key) * 8   # bits
+    a = 12   # rounds
+    b = 8 if variant == "Ascon-128a" else 6   # rounds
+    rate = 16 if variant == "Ascon-128a" else 8   # bytes
+
+    ascon_initialize(S, k, rate, a, b, key, nonce)
+    ascon_process_associated_data(S, b, rate, associateddata)
+    ciphertext = ascon_process_plaintext(S, b, rate, plaintext)
+    tag = ascon_finalize(S, rate, a, key)
+    return ciphertext + tag
+
+def ascon_initialize(S, k, rate, a, b, key, nonce):
+    iv_zero_key_nonce = to_bytes([k, rate * 8, a, b]) + zero_bytes(20-len(key)) + key + nonce
+    S[0], S[1], S[2], S[3], S[4] = bytes_to_state(iv_zero_key_nonce)
+
+    ascon_permutation(S, a)
+
+    zero_key = bytes_to_state(zero_bytes(40-len(key)) + key)
+    S[0] ^= zero_key[0]
+    S[1] ^= zero_key[1]
+    S[2] ^= zero_key[2]
+    S[3] ^= zero_key[3]
+    S[4] ^= zero_key[4]
+
+def ascon_process_associated_data(S, b, rate, associateddata):
+    if len(associateddata) > 0:
+        a_padding = to_bytes([0x80]) + zero_bytes(rate - (len(associateddata) % rate) - 1)
+        a_padded = associateddata + a_padding
+
+        for block in range(0, len(a_padded), rate):
+            S[0] ^= bytes_to_int(a_padded[block:block+8])
+            if rate == 16:
+                S[1] ^= bytes_to_int(a_padded[block+8:block+16])
+
+            ascon_permutation(S, b)
+
+    S[4] ^= 1
+
+def ascon_process_plaintext(S, b, rate, plaintext):
+    p_lastlen = len(plaintext) % rate
+    p_padding = to_bytes([0x80]) + zero_bytes(rate-p_lastlen-1)
+    p_padded = plaintext + p_padding
+
+    # first t-1 blocks
+    ciphertext = to_bytes([])
+    for block in range(0, len(p_padded) - rate, rate):
+        if rate == 8:
+            S[0] ^= bytes_to_int(p_padded[block:block+8])
+            ciphertext += int_to_bytes(S[0], 8)
+        elif rate == 16:
+            S[0] ^= bytes_to_int(p_padded[block:block+8])
+            S[1] ^= bytes_to_int(p_padded[block+8:block+16])
+            ciphertext += (int_to_bytes(S[0], 8) + int_to_bytes(S[1], 8))
+
+        ascon_permutation(S, b)
+
+    # last block t
+    block = len(p_padded) - rate
+    if rate == 8:
+        S[0] ^= bytes_to_int(p_padded[block:block+8])
+        ciphertext += int_to_bytes(S[0], 8)[:p_lastlen]
+    elif rate == 16:
+        S[0] ^= bytes_to_int(p_padded[block:block+8])
+        S[1] ^= bytes_to_int(p_padded[block+8:block+16])
+        ciphertext += (int_to_bytes(S[0], 8)[:min(8,p_lastlen)] + int_to_bytes(S[1], 8)[:max(0,p_lastlen-8)])
+    return ciphertext
+
+def ascon_finalize(S, rate, a, key):
+    assert(len(key) in [16,20])
+    S[rate//8+0] ^= bytes_to_int(key[0:8])
+    S[rate//8+1] ^= bytes_to_int(key[8:16])
+    S[rate//8+2] ^= bytes_to_int(key[16:] + zero_bytes(24-len(key)))
+
+    ascon_permutation(S, a)
+
+    S[3] ^= bytes_to_int(key[-16:-8])
+    S[4] ^= bytes_to_int(key[-8:])
+    tag = int_to_bytes(S[3], 8) + int_to_bytes(S[4], 8)
+    return tag
+
+def ascon_permutation(S, rounds=1):
+    assert(rounds <= 12)
+    for r in range(12-rounds, 12):
+        # --- add round constants ---
+        S[2] ^= (0xf0 - r*0x10 + r*0x1)
+        # --- substitution layer ---
+        S[0] ^= S[4]
+        S[4] ^= S[3]
+        S[2] ^= S[1]
+        T = [(S[i] ^ 0xFFFFFFFFFFFFFFFF) & S[(i+1)%5] for i in range(5)]
+        for i in range(5):
+            S[i] ^= T[(i+1)%5]
+        S[1] ^= S[0]
+        S[0] ^= S[4]
+        S[3] ^= S[2]
+        S[2] ^= 0XFFFFFFFFFFFFFFFF
+        # --- linear diffusion layer ---
+        S[0] ^= rotr(S[0], 19) ^ rotr(S[0], 28)
+        S[1] ^= rotr(S[1], 61) ^ rotr(S[1], 39)
+        S[2] ^= rotr(S[2],  1) ^ rotr(S[2],  6)
+        S[3] ^= rotr(S[3], 10) ^ rotr(S[3], 17)
+        S[4] ^= rotr(S[4],  7) ^ rotr(S[4], 41)
+
+def demo_print(data):
+    maxlen = max([len(text) for (text, val) in data])
+    for text, val in data:
+        print("{text}:{align} 0x{val} ({length} bytes)".format(text=text, align=((maxlen - len(text)) * " "), val=bytes_to_hex(val), length=len(val)))
+
+def bytes_to_hex(b):
+    return b.hex()
+    #return "".join(x.encode('hex') for x in b)
+
+def get_random_bytes(num):
+    import os
+    return to_bytes(os.urandom(num))
+
+def zero_bytes(n):
+    return n * b"\x00"
+
+def to_bytes(l): # where l is a list or bytearray or bytes
+    return bytes(bytearray(l))
+
+def bytes_to_int(bytes):
+    return sum([bi << ((len(bytes) - 1 - i)*8) for i, bi in enumerate(to_bytes(bytes))])
+
+def bytes_to_state(bytes):
+    return [bytes_to_int(bytes[8*w:8*(w+1)]) for w in range(5)]
+
+def int_to_bytes(integer, nbytes):
+    return to_bytes([(integer >> ((nbytes - 1 - i) * 8)) % 256 for i in range(nbytes)])
+
+def rotr(val, r):
+    return (val >> r) | ((val & (1<<r)-1) << (64-r))
+
+host = "192.168.0.12"
+port = 5000
+
+s = socket.socket()
+s.connect((host, port))
+print("Connected to: ", host)
+
+key = get_random_bytes(16) # zero_bytes(keysize)
+nonce = get_random_bytes(16)      # zero_bytes(16)
+
+associated_data = input("Enter the associated data: ")
+associateddata = associated_data.encode('utf-8')
+plain_text = input("Enter the plain text: ")
+plaintext = plain_text.encode('utf-8')
+
+ciphertext = ascon_encrypt(key, nonce, associateddata, plaintext,  "Ascon-128")
+s.send(key)
+s.send(nonce)
+s.send(associateddata)
+s.send(ciphertext)
+    
